976ff01563a1d525ea6ed14fa93f38b277b15951
Zanzambi <zanzambi.oficial@gmail.com>
2023-11-14 14:24:09 +0100

    commit

--- api.js
+++ api.js
@@ -1,3 +1,12 @@
-var h1 = document.querySelector('h1')
+const token ="IGQWRNaXFzSkl6cWtJWm5kcTVxRXltSGRvcUZADeHpZAMmhWTzRTWlFhUC1XTzliT2xsY0pONWZAyRmY1YzZA3MzFvNWtrSHE5ZAkNFR1A1ZAUNtUS1ubnJIdktyNGhWc0htMnlZAQlZAfTnNRSGU0dXNvRFNJVDZAkVkpxaFEZD"
 
-h1.style.fontSize="2em"
+fetch('https://graph.instagram.com/me/media?fields=id,media_url,username,timestamp&access_token=IGQWRNaXFzSkl6cWtJWm5kcTVxRXltSGRvcUZADeHpZAMmhWTzRTWlFhUC1XTzliT2xsY0pONWZAyRmY1YzZA3MzFvNWtrSHE5ZAkNFR1A1ZAUNtUS1ubnJIdktyNGhWc0htMnlZAQlZAfTnNRSGU0dXNvRFNJVDZAkVkpxaFEZD')
+    .then(response => response.json())
+    .then(data => {
+      // faça algo com os dados retornados
+
+      console.log(data)
+    })
+    .catch(error => {
+      // trate erros de requisição
+    });
